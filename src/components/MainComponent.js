774cdb3ec90bff20d4eb13a38739fe131cf4be23
d.tsegtserdem <d.tsegtserdem@gmail.com>
2021-11-26 16:08:46 +0800

    assignment2 task1 aboutus ReactRoute

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,9 +4,14 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Menu from './/MenuComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent'
 import Dishdetail from './DishdetailsComponent';
 import { DISHES } from '../shared/dishes';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
     
@@ -14,8 +19,10 @@ class Main extends Component {
         super(props);
         
         this.state = {
-            dishes: DISHES,
-            selectedDish: null
+          dishes: DISHES,
+          comments: COMMENTS,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
         };
     }
     
@@ -27,9 +34,19 @@ class Main extends Component {
     const HomePage = () => {
       return(
           <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
+    
+    const DishWithId = ({match}) => {
+      return(
+          <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
           
       return (
         <div>
@@ -37,6 +54,9 @@ class Main extends Component {
           <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/aboutus' component={About} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
      function RenderMenuItem ({dish, onClick}) {
         return (
-            <Card
-                onClick={() => onClick(dish.id)}>
+            <Card>
+            <Link to={`/menu/${dish.id}`}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
+            </Link>
             </Card>
         );
     }
@@ -18,13 +20,23 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
         const menu = props.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                    <RenderMenuItem dish={dish} />
                 </div>
             );
         });
 
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
                 <div className="row">
                     {menu}
                 </div>
@@ -33,33 +45,4 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
     }
     
 export default Menu;
-//class Menu extends Component {
-//
-//    render() {
-//            
-//        const menu = this.props.dishes.map((dish) => {
-//            return (
-//              <div  className="col-12 col-md-5 m-1">
-//                <Card onClick={() => this.props.onClick(dish.id)}>
-//                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-//                  <CardImgOverlay>
-//                      <CardTitle>{dish.name}</CardTitle>
-//                  </CardImgOverlay>
-//                </Card>
-//              </div>
-//            );
-//        });
-//
-//        console.log('Menu Component render is invoked');
-//        return (
-//            <div className="container">
-//                <div className="row">
-//                    {menu}
-//                </div>
-//            </div>
-//        );
-//        }
-//    }
-//
-//export default Menu;
     
