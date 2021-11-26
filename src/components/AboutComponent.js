1983df034c48eae8a7f944e6829b0367f5c210bc
d.tsegtserdem <d.tsegtserdem@gmail.com>
2021-11-26 16:31:50 +0800

    assignmet 2 done

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -6,7 +6,20 @@ function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            <React.Fragment>
+                <div className="col-12 mt-5">
+                    <Media tag="li">
+                        <Media left middle>
+                            <Media object src={leader.image} alt={leader.name} />
+                        </Media>
+                        <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                        </Media>
+                    </Media>
+                </div>
+            </React.Fragment>
         );
     });
 
